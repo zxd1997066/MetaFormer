e739cede443e84e31a5b7b22d7146b44baf57148
zengxian <xiangdong.zeng@intel.com>
2023-05-23 16:26:17 +0800

    fp16

--- inference.py
+++ inference.py
@@ -190,7 +190,7 @@ if __name__ == '__main__':
                         img_path=args.img_path, meta_data_path=args.meta_path)
             elif args.precision == "float16":
                 print('---- Enable AMP float16')
-                with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
+                with torch.cpu.amp.autocast(enabled=True, dtype=torch.half):
                     result = Inference(config_path=args.cfg, model_path=args.model_path).infer(
                         img_path=args.img_path, meta_data_path=args.meta_path)
             else:
@@ -204,7 +204,7 @@ if __name__ == '__main__':
                     img_path=args.img_path, meta_data_path=args.meta_path)
         elif args.precision == "float16":
             print('---- Enable AMP float16')
-            with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
+            with torch.cpu.amp.autocast(enabled=True, dtype=torch.half):
                 result = Inference(config_path=args.cfg, model_path=args.model_path).infer(
                     img_path=args.img_path, meta_data_path=args.meta_path)
         else:
